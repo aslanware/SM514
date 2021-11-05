1d71b489196bce8219f1d2c24a48aede65986141
kaptanileyla <aslanm82@yahoo.com>
2021-11-05 14:25:55 +0300

    whitespaces

--- src/main/java/org/sm514/hw1/LastZero.java
+++ src/main/java/org/sm514/hw1/LastZero.java
@@ -1,12 +1,17 @@
 package org.sm514.hw1;
 
 public class LastZero {
+
     public static int lastZero (int[] x) {
+
         for (int i = 0; i < x.length; i++) {
             if (x[i] == 0) {
                 return i;
             }
         }
+
         return -1;
+
     }
+
 }
